48b34f0bfe6a7ca32dbd767ae94fedff26778e9d
EviWang <525205537@qq.com>
2024-03-10 13:07:54 +0800

    fix: add home page

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export default function Home() {
+  return (
+    <main className="bg-black home">
+      <div className="grid grid-cols-2 gap-5">
+        aaa
+      </div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,55 +1,154 @@
-import Card from "@/components/Card";
+import './index.scss';
 
 export default function Home() {
-  const cards = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1619878796466-b7982b1d9263?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      avatarImage: "https://i.pravatar.cc/300?img=1",
-      title: "Go to zoo",
-      date: "Jul.18th.2020",
-      location: "New Zealand",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1619878796466-b7982b1d9263?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      avatarImage: "https://i.pravatar.cc/300?img=2",
-      title: "Attend wedding",
-      date: "Jul.18th.2020",
-      location: "Hawaii",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1619878796466-b7982b1d9263?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      avatarImage: "https://i.pravatar.cc/300?img=3",
-      title: "Mountain hiking",
-      date: "Jul.18th.2020",
-      location: "Malibu",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1619878796466-b7982b1d9263?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      avatarImage: "https://i.pravatar.cc/300?img=4",
-      title: "Attend birthday party",
-      date: "Jul.18th.2020",
-      location: "Hawaii",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1619878796466-b7982b1d9263?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      avatarImage: "https://i.pravatar.cc/300?img=5",
-      title: "Baking",
-      date: "Jul.18th.2020",
-      location: "Pasadena",
-    },
-  ];
-
   return (
-    <main className="bg-blue-100 text-xs text-dark-gray">
-      <div className="grid grid-cols-2 gap-5">
-        {cards.map((card, index) => (
-          <Card key={index} {...card} />
-        ))}
+    <main className="bg-black home">
+      <div className='line-vertical'></div>
+      <div className='top-bar'>
+        <div className='brand-word'>
+            DNA LAB
+        </div>
+        <div className='search'>
+            <div className='icon-search'></div>
+            <input />
+        </div>
+        <div className='logo'>
+
+        </div>
+      </div>
+
+      <div className='main'>
+        <div className='left-nav'>
+            <div className='nav-item active'>
+                <div className='icon shoes'></div>
+            </div>
+            <div className='nav-item '>
+                <div className='icon calendar'></div>
+            </div>
+            <div className='nav-item '>
+                <div className='icon star'></div>
+            </div>
+        </div>
+        
+        <div className='product-main'>
+            <div  className='product-list-wrapper'>
+              <div className="product-list-scroll-box">
+                <div className='product-list'>
+                  <div className='product-item'>
+                    <img src='/shoe-1.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-2.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-3.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-4.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-5.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-1.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-2.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-3.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-4.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/shoe-5.png'></img>
+                  </div>
+                </div>
+              </div>
+              <span className="product-name">
+                Shoe Laces
+              </span>
+            </div>
+            <div  className='product-list-wrapper'>
+            <div className="product-list-scroll-box">
+                <div className='product-list'>
+                  <div className='product-item'>
+                    <img src='/swoosh-1.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-2.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-3.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-4.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-5.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-1.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-2.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-3.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-4.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/swoosh-5.png'></img>
+                  </div>
+                </div>
+              </div>
+              <span className="product-name">
+                Swoosh
+              </span>
+            </div>
+            <div  className='product-list-wrapper'>
+            <div className="product-list-scroll-box">
+                <div className='product-list'>
+                  <div className='product-item'>
+                    <img src='/tongue-1.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-2.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-3.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-4.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-5.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-1.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-2.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-3.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-4.png'></img>
+                  </div>
+                  <div className='product-item'>
+                    <img src='/tongue-5.png'></img>
+                  </div>
+                </div>
+              </div>
+              <span className="product-name">
+                Tongue
+              </span>
+            </div>
+        </div>
       </div>
     </main>
   );
