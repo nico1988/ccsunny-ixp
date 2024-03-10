ec9aa987810ff6f829268ffb0a03eb8723fd7deb
EviWang <525205537@qq.com>
2024-03-10 17:09:40 +0800

    feat: add page 27

---
+++ app/page_27/page.js
@@ -0,0 +1,28 @@
+import './index.scss';
+
+export default function Page27() {
+  return (
+    <main className="bg-black page_27">
+      <div  className='logo'>
+
+      </div>
+
+      <div className='title'>
+        Toebox
+      </div>
+
+      <div  className='shoes'>
+        <span className='text'>Nike Air Force 1 off white -yellow</span>
+        <img src='/page-27/shoes.png'/>
+      </div>
+
+      <img className='shoes-detail' src='/page-27/shoes-detail.png'/>
+
+      <div  className='right-bg'>
+
+      </div>
+
+      <img className='footer' src='/page-27/footer.png'/>
+    </main>
+  );
+}
---
+++ app/page_4/page.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+export default function Page4() {
+  return (
+    <main className="bg-black page_4">
+     
+    </main>
+  );
+}
