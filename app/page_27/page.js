df5f4f7d6297a528e250c641e260aafa2306ff30
EviWang <525205537@qq.com>
2024-03-24 18:47:32 +0800

    feat: 调整为tailwind

--- app/page_27/page.js
+++ app/page_27/page.js
@@ -1,33 +1,39 @@
-import './index.scss';
 
 export default function Page27() {
   return (
-    <main className="bg-black page_27">
-      <div className='line-vertical-1'></div>
-      <div className='line-vertical-2'></div>
-      <div className='line-horizontal-1'></div>
-      <div className='line-horizontal-2'></div> 
-      <div className='line-horizontal-3'></div>
-      <div  className='logo'>
+    <div class="text-white bg-black h-screen relative">
+      <div class="absolute h-screen w-[1px] bg-[#00FFE0] left-[147px] z-10"></div>
+      <div class="absolute h-screen w-[1px] bg-[#00FFE0] left-[1278px] z-10"></div>
 
-      </div>
+      <div class="absolute w-screen h-[1px] bg-[#00FFE0] top-[198px] z-10"></div>
+      <div class="absolute w-screen h-[1px] bg-[#00FFE0] top-[571px] z-10"></div>
+      <div class="absolute w-screen h-[1px] bg-[#00FFE0] top-[916px] z-10"></div>
 
-      <div className='title'>
+      <div class="bg-[url('/nike-logo.png')] bg-no-repeat w-[100px] h-[100px] ml-[24px]"></div>
+
+      <div class="absolute font-bold text-[24px] top-[116px] left-[539px] z-10">
         Toebox
       </div>
 
-      <div  className='shoes'>
-        <span className='text'>Nike Air Force 1 off white -yellow</span>
-        <img src='/page-27/shoes.png'/>
+      <div class="absolute top-0 left-[626px] h-[620px] w-[700px]">
+        <div class="font-bold text-[24px] absolute top-[64px] left-[240px] w-[374px]">
+          Nike Air Force 1 off white -yellow
+        </div>
+        <img src="/page-27/shoes.png" alt="Shoes" class="w-full" />
       </div>
 
-      <img className='shoes-detail' src='/page-27/shoes-detail.png'/>
+      <img
+        src="/page-27/shoes-detail.png"
+        class="absolute top-[216px] left-[170px] w-[248px]"
+      ></img>
 
-      <div  className='right-bg'>
+      <div class="absolute bg-[rgba(178,230,221,1)] w-[80px] h-[629px] right-0 top-0 rounded-bl-[80px]"></div>
 
-      </div>
-
-      <img className='footer' src='/page-27/footer.png'/>
-    </main>
+      <img
+        src="/page-27/footer.png"
+        class="absolute bottom-[100px] left-[144px] h-[230px] w-[1138px]"
+      ></img>
+      <img src="/page-27/footer-circle.svg" class="absolute bottom-[50px] left-[50%] ml-[-120px] h-[78px] w-[240px]"/>
+    </div>
   );
 }
